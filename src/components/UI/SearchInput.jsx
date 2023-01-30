1c1a927545c801445eb776a2d0580c9b95d1f54e
Alibek <alibeknurtuleu@gmail.com>
2023-01-30 13:40:09 +0600

    search input

---
+++ src/components/UI/SearchInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from '../../styles/UI/SearchInput.module.css'
+
+const SearchInput = () => {
+  return (
+    <div className={classes.input}>
+        
+    </div>
+  )
+}
+
+export default SearchInput
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import classes from '../styles/Search.module.css'
 import LoadingAnimation from "../components/UI/Loading";
+import SearchInput from "../components/UI/SearchInput";
 
 const Search = () => {
     return (
         <div className={classes.search}>
             <div className={classes.loading}>
                 <LoadingAnimation></LoadingAnimation>
+                <SearchInput/>
             </div>
         </div>
     )
